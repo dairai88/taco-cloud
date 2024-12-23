3bb4e60356ff8116cf16634f960c831ed4f7b354
Dalei Sun <sundalei2011@163.com>
2024-12-23 21:06:44 +0800

    update ci to use maven base image

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-
 import lombok.extern.slf4j.Slf4j;
 import tacos.data.IngredientRepository;
 import tacos.domain.Ingredient;
